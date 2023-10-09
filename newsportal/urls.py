e04385ed8dd5a80f822b566b4fe9627f8edf0a06
Gaurav Pingale <gauravpingale4@gmail.com>
2023-10-10 00:15:11 +0530

    Admin pannel title test changes

--- newsportal/urls.py
+++ newsportal/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "News Portal"
+admin.site.site_title = "News Portal Admin"
+admin.site.index_title = "Welcome to News Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("app.urls"))
